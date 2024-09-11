f729f152bcf3f6e592146524c93e070d20065bf3
matthew <matthew@wgolden.com>
2024-09-11 08:45:31 -0500

    functions to compare previous two files

--- lambda/dpd_active_calls_download_event_handler/app.py
+++ lambda/dpd_active_calls_download_event_handler/app.py
@@ -130,6 +130,50 @@ def update_addresses(json_data, update_date):
             enqueue(address_dict)
         put_record(record=record)
 
+def last_two_files(bucket, prefix):
+    s3 = boto3.client('s3')
+    s3_objects = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    result = s3_objects['Contents']
+    while s3_objects.get('NextContinuationToken'):
+        token = s3_objects['NextContinuationToken']
+        s3_objects = s3.list_objects_v2(Bucket=bucket, Prefix=prefix, ContinuationToken=token)
+        result = result + s3_objects['Contents']
+    sorted_results = sorted(result, key=lambda o: o['LastModified'])
+    sorted_results = [result for result in sorted_results if result['Size'] > 0]
+    last_two = sorted_results[-2:]
+    for file in last_two:
+        file['body'] = read_file((bucket, file['Key']))['body']
+    return last_two
+
+def columns_are_same(key, cur_file, prev_file):
+    incident_number = key[0]
+    unit_number = key[1]
+    cur_row = []
+    prev_row = []
+    for f in cur_file['body']:
+        if f['incident_number'] == incident_number and f['unit_number'] == unit_number:
+            cur_row = list(f.values())
+
+    for f in prev_file['body']:
+        if f['incident_number'] == incident_number and f['unit_number'] == unit_number:
+            prev_row = list(f.values()) 
+            
+    cur_hash = sha1('|'.join(cur_row).encode(encoding='utf-8')).hexdigest()
+    prev_hash = sha1('|'.join(prev_row).encode(encoding='utf-8')).hexdigest()
+    return cur_hash == prev_hash
+
+def compare_files(cur_file, prev_file):
+    cur = set([(item['incident_number'], item['unit_number']) for item in cur_file['body']])
+    prev = set([(item['incident_number'], item['unit_number']) for item in prev_file['body']])
+    to_delete = prev - cur
+    to_add = cur - prev
+    to_update = set([item for item in cur & prev if not columns_are_same(item, cur_file, prev_file)])
+    return {
+        'to_delete': to_delete,
+        'to_add': to_add,
+        'to_update': to_update
+    }
+
 
 def lambda_handler(event, context):
     events = get_records(event)
@@ -139,6 +183,14 @@ def lambda_handler(event, context):
         download_date = json_data['as_of']
         records = json_data['body']
         update_addresses(records, download_date)
+
+        for file in files:
+     ...:     tuples = set()
+     ...:     for item in file['body']:
+     ...:         tuples.add(tuple([item['incident_number'], item['unit_number']]))
+     ...:     file['tuples'] = tuples
+
+
         
 
 
