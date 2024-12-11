fcb4e4d831123cf37e1fd25b0a6ef1ce432a8ece
matthew <matthew@wgolden.com>
2024-12-11 06:10:16 -0600

    Remove address hashmap. Add logic to merge call events

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -4,40 +4,103 @@ import Map from './map.js'
 
 function app() {
     const map = new Map('map', { minZoom: 10 })
-    let activeCalls = []
-    let addresses = {}
+    let callMap = {}
 
     function getCurrentCalls(data) {
-        activeCalls = data.current_active_calls
-        addresses = activeCalls.map(call => {
-            let addr = call.address
-            if(addr.length > 0 && addr[0] !== null) {
-                return {"call_id": call.call_id, "address_id": call.address_id, "address": addr[0]}
-            }
+        data.current_active_calls.forEach(call => {
+            const call_id = call.call_id
+            const addr = call.address
+            callMap[call_id] = call
         })
-        .filter(Boolean)
         updateMap()
     }
 
     function updateMap() {
-        const addressArray = addresses.map(addr => { 
-            return {"call_id": addr.call_id,"address_id": addr.address_id, "coords": [addr.address.latitude, addr.address.longitude]}  
+        const addressArray = Object.keys(callMap).map(key => {
+            const call = callMap[key]
+            const address = call.address
+            if(address !== null && address.length > 0) {
+                return {"call_id": key,"address_id": call.address_id, "coords": [address[0].latitude, address[0].longitude]}
+            }
         })
         map.add_markers(addressArray, markerClickHandler)
     }
 
     function markerClickHandler(marker) {
-        console.log(marker)
+        const call_id = marker["call_id"]
+        const call = callMap[call_id]
+        console.log(call)
     }
 
-    function eventStreamHandler(data) {
+    function eventStreamHandler(evt) {
         console.log("in event stream handler")
-        console.log(data)
+        console.log(typeof evt)
+        const jsonObj = JSON.parse(evt.replace(/'/g, '"'))
+        console.log(jsonObj)
+        try {
+            const event_type = jsonObj["event"]
+            const data = jsonObj["data"]
+            if(event_type === "call_changes") {
+                mergeCallEvents(data)
+            }
+            if(event_type === "address_changes") {
+                mergeAddressEvents(data)
+            }
+        }
+        catch (error) {
+            console.error("Error parsing JSON string: ", error)
+        }
     }
+
+    function mergeCallEvents(callEvents) {
+        console.log("merge call events")
+        callEvents.forEach(call => {
+            const changeType = call.change_type
+            if(changeType === "add"){
+                addCall(call)
+            }
+            if(changeType === "delete"){
+                deleteCall(call)
+            }
+            if(changeType === "update"){
+                updateCall(call)
+            }
+        })
+    }
+
+    function deleteCall(call) {
+        const call_id = call.call_id
+        map.remove_marker(call_id)
+        delete callMap[call_id]
+    }
+
+    function addCall(call) {
+        const call_id = call.call_id
+        callMap[call_id] = call
+    }
+
+
+    function updateCall(newCall) {
+        const call_id = newCall.call_id
+        const curCall = callMap[call_id]
+        if(curCall) {
+            Object.keys(newCall).forEach(key => {
+                if(curCall[key] !== newCall[key]) {
+                    curCall[key] = newCall[key]
+                }
+            })
+        }
+    }
+
+    function mergeAddressEvents(addressEvent) {
+        console.log(addressEvent)
+    }
+    
 
     function initialize() {
         try {
             currentCalls(getCurrentCalls)
+            updateMap()
             subscribeToEvents(eventStreamHandler)            
         }
         catch (error) {
