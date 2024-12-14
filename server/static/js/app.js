6553c58c629c9dbf3f902fbfd962e07cf57b6513
matthew <matthew@wgolden.com>
2024-12-14 08:29:35 -0600

    Update app to use new classes

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -1,99 +1,41 @@
 import { currentCalls, subscribeToEvents } from './api.js'
 import Map from './map.js'
+import { ActiveCalls, Call, Location } from './calls.js'
+import { EventStreamHandler } from './eventStreamHandler.js'
 
 
 function app() {
     const map = new Map('map', { minZoom: 10 })
-    let callMap = {}
+    const calls = new ActiveCalls()
+    const streamHandler = new EventStreamHandler(calls, map)
 
     function getCurrentCalls(data) {
-        data.current_active_calls.forEach(call => {
-            const call_id = call.call_id
-            const addr = call.address
-            callMap[call_id] = call
+        data.current_active_calls.forEach(activeCall => {
+            const call = new Call(activeCall)
+            calls.addCall(call)
+            if("address" in activeCall) {
+                const address = activeCall.address
+                if(address.length > 0){
+                    const location = new Location({
+                        "locationId": activeCall.address_id,
+                        "coords": [address[0].latitude, address[0].longitude]
+                    })
+                    calls.addLocation(location)
+                }
+            }
         })
         updateMap()
     }
 
     function updateMap() {
-        const addressArray = Object.keys(callMap).map(key => {
-            const call = callMap[key]
-            const address = call.address
-            if(address !== null && address.length > 0) {
-                return {"call_id": key,"address_id": call.address_id, "coords": [address[0].latitude, address[0].longitude]}
-            }
-        })
-        map.add_markers(addressArray, markerClickHandler)
+        const locations = calls.getLocations()
+        map.add_markers(locations, markerClickHandler)
     }
 
     function markerClickHandler(marker) {
-        const call_id = marker["call_id"]
-        const call = callMap[call_id]
+        const callId = marker["callId"]
+        const call = calls.getCallById(callId)
         console.log(call)
-    }
-
-    function eventStreamHandler(evt) {
-        console.log("in event stream handler")
-        console.log(typeof evt)
-        const jsonObj = JSON.parse(evt.replace(/'/g, '"'))
-        console.log(jsonObj)
-        try {
-            const event_type = jsonObj["event"]
-            const data = jsonObj["data"]
-            if(event_type === "call_changes") {
-                mergeCallEvents(data)
-            }
-            if(event_type === "address_changes") {
-                mergeAddressEvents(data)
-            }
-        }
-        catch (error) {
-            console.error("Error parsing JSON string: ", error)
-        }
-    }
-
-    function mergeCallEvents(callEvents) {
-        console.log("merge call events")
-        callEvents.forEach(call => {
-            const changeType = call.change_type
-            if(changeType === "add"){
-                addCall(call)
-            }
-            if(changeType === "delete"){
-                deleteCall(call)
-            }
-            if(changeType === "update"){
-                updateCall(call)
-            }
-        })
-    }
-
-    function deleteCall(call) {
-        const call_id = call.call_id
-        map.remove_marker(call_id)
-        delete callMap[call_id]
-    }
-
-    function addCall(call) {
-        const call_id = call.call_id
-        callMap[call_id] = call
-    }
-
-
-    function updateCall(newCall) {
-        const call_id = newCall.call_id
-        const curCall = callMap[call_id]
-        if(curCall) {
-            Object.keys(newCall).forEach(key => {
-                if(curCall[key] !== newCall[key]) {
-                    curCall[key] = newCall[key]
-                }
-            })
-        }
-    }
-
-    function mergeAddressEvents(addressEvent) {
-        console.log(addressEvent)
     }
     
 
@@ -101,7 +43,7 @@ function app() {
         try {
             currentCalls(getCurrentCalls)
             updateMap()
-            subscribeToEvents(eventStreamHandler)            
+            streamHandler.registerEventHandlerWith(subscribeToEvents)         
         }
         catch (error) {
             console.error("Error initializing application", error)
