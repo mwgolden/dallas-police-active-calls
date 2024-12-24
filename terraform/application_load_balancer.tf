b4bd1e00c126df51b824f48fceb5bad1dbf61b83
matthew <matthew@wgolden.com>
2024-12-24 08:07:53 -0600

    Add certificate and redirect http to https

--- terraform/application_load_balancer.tf
+++ terraform/application_load_balancer.tf
@@ -19,6 +19,25 @@ resource "aws_lb_listener" "dpd_active_calls_lb_listener" {
     load_balancer_arn = aws_lb.dpd_active_calls_lb.arn
     port = "80"
     protocol = "HTTP"
+    depends_on = [ aws_lb_target_group.dpd_active_calls_target_group ]
+
+    default_action {
+      type = "redirect"
+      redirect {
+        port = 443
+        protocol = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+}
+
+resource "aws_lb_listener" "https" {
+    load_balancer_arn = aws_lb.dpd_active_calls_lb.arn
+    port = 443
+    protocol = "HTTPS"
+    ssl_policy = "ELBSecurityPolicy-2016-08"
+    certificate_arn = local.aws_acm_certificate_arn
+    depends_on = [ aws_lb_target_group.dpd_active_calls_target_group ]
 
     default_action {
       type = "forward"
