3eb5c594e29fd750a180aef118e16af3dc66d0d8
matthew <matthew@wgolden.com>
2025-06-29 09:59:00 -0500

    Validate api key for events endpoint

--- server/api/v1/active_calls_router.py
+++ server/api/v1/active_calls_router.py
@@ -1,14 +1,27 @@
-from fastapi import APIRouter, Request #type: ignore
+from fastapi import APIRouter, Request, Depends, HTTPException, status #type: ignore
 from fastapi.responses import StreamingResponse  #type: ignore
 import os
 import asyncio
 import json
 from api.v1.rate_limiter import limiter
 from police_calls import active_calls
+from fastapi.security.api_key import APIKeyHeader
+
+
+API_KEY_NAME = "X-Api-Key"
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
 router = APIRouter()
 
 event_queue = asyncio.Queue()
+
+async def verify_api_key(api_key: str = Depends(api_key_header)):
+    if api_key != os.getenv("EVENT_PUBLISH_API_KEY"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing api key"
+        )
 
 async def event_publisher():
     while True:
@@ -17,7 +30,7 @@ async def event_publisher():
         await asyncio.sleep(0.1)
 
 @router.post("/events/")
-async def receive_events(request:Request):
+async def receive_events(request:Request, api_key: str = Depends(verify_api_key)):
     event = await request.json()
     await event_queue.put(event)
     return {"message": event}
