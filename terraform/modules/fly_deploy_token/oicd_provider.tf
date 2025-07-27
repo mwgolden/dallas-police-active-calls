7974f7ba16da4c2aae480e964631f6876722436b
matthew <matthew@wgolden.com>
2025-07-27 09:34:13 -0500

    Add oidc role and policy; Update github actions

---
+++ terraform/modules/fly_deploy_token/oicd_provider.tf
@@ -0,0 +1,51 @@
+resource "aws_iam_openid_connect_provider" "github_provider" {
+    url = "https://token.actions.githubusercontent.com"
+    client_id_list = [ "sts.amazonaws.com" ]
+}
+
+
+data "aws_iam_policy_document" "github_oidc_policy" {
+  statement {
+    effect = "Allow"
+    actions = [ "sts:AssumeRoleWithWebIdentity" ]
+    principals {
+      type = "Federated"
+      identifiers = [ "${aws_iam_openid_connect_provider.github_provider.arn}" ]
+    }
+    condition {
+      test = "ForAnyValue:StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values = [ "sts.amazon.com" ]
+    }
+    condition {
+      test = "ForAnyValue:StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [ "repo:dallas-police-active-calls/dallas-police-active-calls:ref:refs/heads/main" ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "lambda_invoke" {
+  statement {
+    effect = "Allow"
+    actions = [ "lambda:InvokeFunction" ]
+    resources = [ "${aws_lambda_function.flyctl_deploy_token_lambda.arn}" ]
+  }
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+    name = "github-oidc-lambda-invoke"
+    path = "/"
+    description = "Grant access to invoke lambda for deployment token"
+    policy = data.aws_iam_policy_document.lambda_invoke.json
+}
+
+resource "aws_iam_role" "github_oidc_lambda" {
+  name = "GithubOIDCLambdaRole"
+  assume_role_policy = data.aws_iam_policy_document.github_oidc_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_lambda_invoke_policy" {
+  role = aws_iam_role.github_oidc_lambda.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
